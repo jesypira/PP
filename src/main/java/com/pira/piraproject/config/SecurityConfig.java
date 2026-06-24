aad2141469b23ca77f7957043c02bb14f29e70c5
jesypira <jessica.cabral.a@gmail.comm>
2026-06-24 21:46:05 +0100

    init

--- src/main/java/com/pira/piraproject/config/SecurityConfig.java
+++ src/main/java/com/pira/piraproject/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
         http
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(List.of("http://localhost:5174"));
+                    config.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:5174"));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
                     config.setAllowCredentials(true);
