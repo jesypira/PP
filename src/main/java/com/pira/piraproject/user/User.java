3dbdfa0c43a88d906a7135dbedb54beda4a34899
jesypira <jessica.cabral.a@gmail.comm>
2026-06-25 13:35:48 +0100

    local cors

--- src/main/java/com/pira/piraproject/user/User.java
+++ src/main/java/com/pira/piraproject/user/User.java
@@ -1,6 +1,7 @@
 package com.pira.piraproject.user;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,7 +24,7 @@ public class User {
     private String username;
 
     @Column(nullable = false)
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     @Builder.Default
